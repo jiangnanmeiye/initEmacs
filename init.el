b7848c755f0b306ab23d7c567b64be10db67b485
jiangnanmeiye <jiangnanmeiye@gmail.com>
2018-10-08 15:32:31 +0800

    modified

--- init.el
+++ init.el
@@ -13,6 +13,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (misterioso)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
