560846128c18c9558d2a8d7ca35f1b492a0be0ef
jiangnanmeiye <jiangnanmeiye@gmail.com>
2018-10-08 15:07:53 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,22 @@
+(package-initialize)
+
+(global-company-mode t)
+(global-linum-mode t)
+(global-eldoc-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq make-backup-files nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Times New Roman" :foundry "outline" :slant italic :weight bold :height 90 :width normal)))))
