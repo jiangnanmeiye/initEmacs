36dab8a3e97f1e875f1a485e3dbf7634e6716314
meiye <1486181677@qq.com>
2018-10-10 00:22:58 +0800

    Update init.el

--- init.el
+++ init.el
@@ -1,14 +1,45 @@
-(package-initialize)
+(when (>= emacs-major-version 24)
+     (require 'package)
+     (package-initialize)
+     (setq package-archives '(("gnu"."http://elpa.emacs-china.org/gnu/")
+		      ("melpa"."http://elpa.emacs-china.org/melpa/"))))
 
-(global-company-mode t)
-(global-linum-mode t)
-(scroll-bar-mode -1)
-(global-eldoc-mode -1)
-(tool-bar-mode -1)
-(global-hl-line-mode t)
-(delete-selection-mode t)
+;; cl - Common Lisp Extension
+(require 'cl)
+
+;; Add Packages
+(defvar my/packages '(
+		;; --- Auto-completion ---
+		company
+		;; --- Better Editor ---
+		hungry-delete
+		swiper
+		counsel
+		smartparens
+		;; --- Major Mode ---
+		js2-mode
+		;; --- Minor Mode ---
+		nodejs-repl
+		exec-path-from-shell
+		;; solarized-theme
+		) "Default packages")
+
+(setq package-selected-packages my/packages)
+
+(defun my/packages-installed-p ()
+     (loop for pkg in my/packages
+	   when (not (package-installed-p pkg)) do (return nil)
+	   finally (return t)))
+
+(unless (my/packages-installed-p)
+     (message "%s" "Refreshing package database...")
+     (package-refresh-contents)
+     (dolist (pkg my/packages)
+       (when (not (package-installed-p pkg))
+	 (package-install pkg))))
 
 (require 'org)
+(require 'hungry-delete)
 (setq org-src-fontify-natively t)
 (setq inhibit-splash-screen t)
 (setq make-backup-files nil)
@@ -26,17 +57,25 @@
 
 (global-set-key (kbd "<f1>") 'open-config-file)
 
+(global-company-mode t)
+(global-linum-mode t)
+(global-hungry-delete-mode t)
+(scroll-bar-mode -1)
+(global-eldoc-mode -1)
+(tool-bar-mode -1)
+(global-hl-line-mode t)
+(delete-selection-mode t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (misterioso)))
- '(package-selected-packages (quote (company)))
+ '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier New" :foundry "outline" :slant italic :weight normal :height 98 :width normal)))))
+ '(default ((t (:family "Courier New" :foundry "outline" :slant italic :weight bold :height 98 :width normal)))))
