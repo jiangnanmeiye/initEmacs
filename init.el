35af84559ac013515f12695164e8ede8c6b98c15
jiangnanmeiye <jiangnanmeiye@gmail.com>
2018-10-10 10:35:00 +0800

    Update init.el

--- init.el
+++ init.el
@@ -16,6 +16,7 @@
 		swiper
 		counsel
 		smartparens
+		paredit
 		;; --- Major Mode ---
 		js2-mode
 		;; --- Minor Mode ---
@@ -41,12 +42,12 @@
 (require 'hungry-delete)
 (require 'smartparens-config)
 (require 'nodejs-repl)
+(require 'recentf)
 (setq org-src-fontify-natively t)
 (setq inhibit-splash-screen t)
 (setq make-backup-files nil)
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 (setq-default cursor-type 'bar)
-(require 'recentf)
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
@@ -59,7 +60,7 @@
 
 (defun open-config-file ()
   (interactive)
-  (find-file "c:/users/admin/AppData/Roaming/.emacs.d/init.el"))
+  (find-file "c:/users/koal/AppData/Roaming/.emacs.d/init.el"))
 
 (global-set-key (kbd "<f1>") 'open-config-file)
 
@@ -79,6 +80,7 @@
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "C-h f") 'counsel-describe-function)
 (global-set-key (kbd "C-h v") 'counsel-describe-variable)
 
@@ -88,6 +90,55 @@
        '(("\\.js\\'" . js2-mode))
        auto-mode-alist))
 
+;;;;;;;;;;;;
+;;Copy the configuration about Scheme from 'http://yinwang.org'
+;;;;;;;;;;;;
+(require 'cmuscheme)
+(setq scheme-program-name "scheme") ;;If use Petite, change the variable scheme-program-name to "petite"
+
+;; bypass the interactive question and start the default interpreter
+(defun scheme-proc ()
+  "Return the current Scheme process, starting one if necessary."
+  (unless (and scheme-buffer
+               (get-buffer scheme-buffer)
+               (comint-check-proc scheme-buffer))
+    (save-window-excursion
+      (run-scheme scheme-program-name)))
+  (or (scheme-get-process)
+      (error "No current process. See variable `scheme-buffer'")))
+
+(defun scheme-split-window ()
+  (cond
+   ((= 1 (count-windows))
+    (delete-other-windows)
+    (split-window-vertically (floor (* 0.68 (window-height))))
+    (other-window 1)
+    (switch-to-buffer "*scheme*")
+    (other-window 1))
+   ((not (find "*scheme*"
+               (mapcar (lambda (w) (buffer-name (window-buffer w)))
+                       (window-list))
+               :test 'equal))
+    (other-window 1)
+    (switch-to-buffer "*scheme*")
+    (other-window -1))))
+
+(defun scheme-send-last-sexp-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-last-sexp))
+
+(defun scheme-send-definition-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-definition))
+
+(add-hook 'scheme-mode-hook
+  (lambda ()
+    (paredit-mode 1)
+    (define-key scheme-mode-map (kbd "<f5>") 'scheme-send-last-sexp-split-window)
+    (define-key scheme-mode-map (kbd "<f6>") 'scheme-send-definition-split-window)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -96,6 +147,9 @@
  '(company-idle-delay 0.08)
  '(company-minimum-prefix-length 1)
  '(custom-enabled-themes (quote (misterioso)))
+ '(package-selected-packages
+   (quote
+    (paredit company hungry-delete swiper counsel smartparens js2-mode nodejs-repl)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
